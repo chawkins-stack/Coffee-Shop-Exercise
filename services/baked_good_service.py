2bbd53caa7f861ca5ea8ad0bc22d86bf9befd637
Ciera Watts <cwatts@catalyte.io>
2026-07-13 14:16:29 -0500

    Implement CRUD methods in Baked_goodService class

--- services/baked_good_service.py
+++ services/baked_good_service.py
@@ -10,5 +10,21 @@ class Baked_goodService:
         if self._repository.get_by_id(baked_good.name) is not None:
             raise DuplicateBakedGoodError(f"Baked good '{baked_good.name}' already exists.")
         return self._repository.add(baked_good)
+    
+    def get_all_baked_goods(self) -> list[Baked_good]:
+        return self._repository.get_all()
+    
+    def get_by_id(self, name: str) -> Baked_good:
+        return self._repository.get_by_id(name)
+    
+    def add_baked_good(self, baked_good: Baked_good) -> Baked_good:
+        return self._repository.add(baked_good)
+    
+    def update_baked_good(self, baked_good: Baked_good) -> Baked_good:
+        return self._repository.update(baked_good)
+    
+    def delete_baked_good(self, name: str) -> None:
+        self._repository.delete(name)
+        
 
  
