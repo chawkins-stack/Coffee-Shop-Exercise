ab2017225fa079b265018f9d584c58bf9bb950f5
Ciera Watts <cwatts@catalyte.io>
2026-07-13 10:09:29 -0500

    Add name, purchasing cost, marking percentage, and vendor name class to models/baked_good.py file

---
+++ models/baked_good.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
+class Ingredient (
+    name:"Cookie",
+    purchasing_cost: "8.00"
+    marking_percentage:"30.00"
+    vendor_name: "Insomnia Cookies"
+)
