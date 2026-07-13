975658374f351e14b6c57132a00b587bc2b145e8
Ciera Watts <cwatts@catalyte.io>
2026-07-13 14:36:28 -0500

    Add PurchaseService class with CRUD methods and duplicate check

--- services/purchase_service.py
+++ services/purchase_service.py
@@ -1,1 +1,27 @@
-purchase
+import datetime
+
+from repositories.purchase import Purchase_repository
+from models.purchase import Purchase
+from exceptions import DuplicatePurchaseError
+
+class PurchaseService:
+    def __init__(self, repository: Purchase_repository):
+        self._repository = repository
+
+    def create_purchase(self, purchase: Purchase) -> Purchase:
+        if self._repository.get_by_id(purchase.timestamp) is not None:
+            raise DuplicatePurchaseError(f"Purchase with timestamp '{purchase.timestamp}' already exists.")
+        return self._repository.add(purchase)
+    
+    def get_all_purchases(self) -> list[Purchase]:
+        return self._repository.get_all()
+    
+    def get_purchase_by_timestamp(self, timestamp: datetime) -> Purchase | None:
+        return self._repository.get_by_id(timestamp)
+    
+    def update_purchase(self, timestamp: datetime, purchase: Purchase) -> Purchase | None:
+        return self._repository.update(timestamp, purchase)
+    
+    def delete_purchase(self, timestamp: datetime) -> bool:
+        return self._repository.delete(timestamp)
+    
