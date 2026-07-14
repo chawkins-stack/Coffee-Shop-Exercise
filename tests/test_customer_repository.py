e2588e6bbae39f650ea4291db8a67323a6dace1a
Brennan Idor <bidor@catalyte.io>
2026-07-14 15:46:12 -0500

    Created three test cases for get_by_email for CustomerRepository

--- tests/test_customer_repository.py
+++ tests/test_customer_repository.py
@@ -4,19 +4,28 @@ def test_get_all_customer_dataset():
     assert customer_dataset().get_all() == [c_00, c_01, c_02, c_03, c_04, c_05, c_06, c_07, c_08, c_09]
 
 def test_get_by_id_is_c_04():
-    assert customer_dataset().get_by_id(2004) == c_04
+    assert customer_dataset().get_by_id(2004) is c_04
 
 def test_get_by_id_is_c_05():
-    assert customer_dataset().get_by_id(2005) == c_05
+    assert customer_dataset().get_by_id(2005) is c_05
 
-def test_get_by_id_is_none():
+def test_get_by_id_nonexistent_id():
     assert customer_dataset().get_by_id(9898) is None
 
 def test_get_by_name_is_c_03():
-    assert customer_dataset().get_by_name("Arlette") == c_03
+    assert customer_dataset().get_by_name("Arlette") is c_03
 
 def test_get_by_name_is_c_06():
-    assert customer_dataset().get_by_name("Jarquetta") == c_06
+    assert customer_dataset().get_by_name("Jarquetta") is c_06
 
-def test_get_by_name_is_none():
+def test_get_by_name_nonexistent_name():
     assert customer_dataset().get_by_name("LeBron James") is None
+
+def test_get_by_email_is_c_02():
+    assert customer_dataset().get_by_email("bidor@catalyte.io") is c_02
+
+def test_get_by_email_is_c_07():
+    assert customer_dataset().get_by_email("ocasselle@sdipresence.com") is c_07
+
+def test_get_by_email_nonexistent_email():
+    assert customer_dataset().get_by_email("johnny.appleseed@gmail.com") is None
