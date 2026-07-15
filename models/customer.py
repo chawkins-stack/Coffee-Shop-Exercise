272d53528caf5607bbff8108664126ea663a7327
chawkins-stack <chawkins@catalyte.io>
2026-07-15 13:30:27 -0500

    refactor: Remove custom __init__ method from Customer class for dataclass compatibility

--- models/customer.py
+++ models/customer.py
@@ -5,14 +5,7 @@ from decimal import Decimal
 
 @dataclass
 class Customer:
-<<<<<<< HEAD
-  def __init__(self, id: int, name:str,email: str):
-    self.id = id
-    self.name = name
-    self.email = email
-=======
     name: str
     email: str
     lifetime_spent: Decimal
     id: Optional[Number] = None
->>>>>>> 0b92d7e6288b7a66ad6aca09cead5697d29845c8
