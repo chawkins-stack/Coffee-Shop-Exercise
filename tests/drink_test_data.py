272bbb0d5ab21500a97f91265cc320e8c68cb9a6
Brennan Idor <bidor@catalyte.io>
2026-07-15 11:24:09 -0500

    Saving for pull

--- tests/drink_test_data.py
+++ tests/drink_test_data.py
@@ -2,8 +2,8 @@ from models.drink import Drink
 from repositories.drink_repository import DrinkRepository
 from ingredient_test_data import i_00, i_01, i_02, i_03, i_04, i_05, i_06, i_07, i_08, i_09, i_10, i_11, i_12, i_13, i_14, i_15, i_16, i_17
 
-# d_00 = Drink(
-#     3000,
-#     "Classic Espresso",
-#     []
-# )
+d_00 = Drink(
+    3000,
+    "Classic Espresso",
+    []
+)
