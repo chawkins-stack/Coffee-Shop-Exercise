983639643f31caa3e2768a8a27f00807ff504be3
Ciera Watts <cwatts@catalyte.io>
2026-07-16 12:14:55 -0500

    refactor: Remove unused imports from purchase model

--- models/purchase.py
+++ models/purchase.py
@@ -4,8 +4,7 @@ from decimal import Decimal
 from numbers import Number
 from typing import Optional
 from models.customer import Customer
-from models.drink import Drink
-from models.baked_good import BakedGood
+
 
 @dataclass
 class Purchase:
