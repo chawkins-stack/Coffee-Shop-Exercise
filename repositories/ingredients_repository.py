a787185bae3b04e880e7f304f92d1791395b7d6e
chawkins-stack <chawkins@catalyte.io>
2026-07-16 12:47:23 -0500

    feat: Refactor repositories to ensure consistent ID handling and improve delete method signatures (#102)
    Co-authored-by: Arlette Diaz <adiaz@catalyte.io>

--- repositories/ingredients_repository.py
+++ repositories/ingredients_repository.py
@@ -1,22 +1,22 @@
 # repositories/ingredients_repository.py
 from numbers import Number
 
-from models.ingredients import Ingredient
+from models.ingredient import Ingredient
 
 
 class IngredientRepository:
     def __init__(self):
-        self._ingredients: list[Ingredient] = []
+        self._ingredient: list[Ingredient] = []
         self._next_id = 4901
 
     def get_all(self) -> list[Ingredient]:
-        return self._ingredients
+        return self._ingredient
 
     def get_by_id(self, id: Number) -> Ingredient | None:
-        return next((ingredient for ingredient in self._ingredients if ingredient.id == id), None)
+        return next((ingredient for ingredient in self._ingredient if ingredient.id == id), None)
 
     def get_by_name(self, name: str) -> Ingredient | None:
-        return next((ingredient for ingredient in self._ingredients if ingredient.name == name), None)
+        return next((ingredient for ingredient in self._ingredient if ingredient.name == name), None)
 
     def add(self, ingredient: Ingredient) -> Ingredient:
         ingredient.id = self._next_id
@@ -28,8 +28,10 @@ class IngredientRepository:
     def update(self, id: Number, ingredient: Ingredient) -> Ingredient | None:
         for i, existing_ingredient in enumerate(self._ingredients):
             if existing_ingredient.id == id:
+                ingredient.id = id
                 self._ingredients[i] = ingredient
                 return ingredient
+            return None
 
     def delete(self, id: Number) -> bool:
         ingredient = self.get_by_id(id)
